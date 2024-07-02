889aff363d566dbbe382b7ebe350e71531c3a572
pawans993 <pavank.ic.22@nitj.ac.in>
2024-07-02 10:58:43 +0530

    Update pawan.js

--- pawan.js
+++ pawan.js
@@ -1,1 +1,3 @@
-doc.queryselectorall("div");
+<p>hello every one my self pawan singh i am from ghoriya parsauna ghorawal sonbhdra i had been complete schooling from gov collage mirzapur than after i had take the admission in nit j throught jee main and also i have qualify jee advance but rank is so much low so that i have not find any collage </p>
+<br>
+<p>about the pankaj singh</p>
